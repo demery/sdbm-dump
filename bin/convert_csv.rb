dc7bb33b0f721e02d997771a7e4a9fd46cff0f5e
Doug Emery <emeryr@upenn.edu>
2014-02-13 11:57:53 -0500

    Print out useful progress info

--- bin/convert_csv.rb
+++ bin/convert_csv.rb
@@ -3,16 +3,16 @@
 require 'csv'
 require 'sqlite3'
 require 'active_record'
-
 # require all the ruby files in lib
 Dir[File.expand_path('../../lib',__FILE__) + '/*.rb'].each { |f| require f }
 
-# csv    = Roo::CSV.new File.expand_path('../../data/schoenbergAll.csv', __FILE__)
-# csv    = Roo::CSV.new File.expand_path('../../data/schoenbergSmall.csv', __FILE__)
 csv_file = ARGV.shift
 db_file  = File.expand_path('../../db/sdbm_dump.sqlite3', __FILE__)
 
-File.delete db_file if File.exists?(db_file)
+if File.exists?(db_file)
+  puts "Deleting existing database: #{db_file}"
+  File.delete db_file
+end
 
 class Observation < ActiveRecord::Base; end
 
@@ -23,6 +23,8 @@ ActiveRecord::Base.establish_connection(
 
 CreateObservations.new.change
 
+puts "Created database: #{db_file}"
+
 width = 7
 count = 0
 last_row = %x( wc -l #{csv_file} ).strip.split[0].to_i - 1
@@ -32,8 +34,8 @@ CSV.new(File.open(csv_file, 'r'), headers: true).lazy.each do |row|
   }
   Observation.new(attrs).save!
   if count % 1000 == 0 || row == last_row
-    puts sprintf("%7d of c. %7d added -- %s",count,last_row, Time.now.to_s)
+    puts sprintf("%7d of c. %d added -- %s",count,last_row,Time.now.to_s)
   end
   count += 1
 end 
-puts sprintf("%7d of c. %7d added -- %s",count,last_row, Time.now.to_s)
+puts sprintf("%7d of %7d added -- %s",count,count,Time.now.to_s)
